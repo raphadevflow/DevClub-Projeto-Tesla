8c04192d50ced2b5ec6f0a36b6ea7e9e7102665a
raphadevflow <raphaelgomescontato7@gmail.com>
2024-07-13 03:25:36 -0300

    alterações no index, style e scripts

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "30%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
